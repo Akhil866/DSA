86e2841342b6d03a445a8d82c887d0ae9b3a16fe
Akhil866 <akhilyadavpr430@gmail.com>
2025-08-20 11:56:11 +0530

    recursion

--- recursion.cpp
+++ recursion.cpp
@@ -27,22 +27,70 @@
 
 // print the number from1 to n 
 
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+
+// void f(int i,int n)
+// {
+//     if(i > n )
+//     return;
+//     cout << i<< endl;
+
+//     f(i+1,n);
+// }
+
+// int main (){
+//     int n,i =1;
+//     cin>>n;
+
+//     f(i,n);
+//      return 0;
+// }
+
+
+/*print the numbrer from n to 1*/
+
+// #include <iostream>
+// using namespace std;
+// void f(int n){
+//     int i = n;
+
+//     if(i>0)
+//     {
+//         cout << i<<endl;
+//         f(i-1);
+//     }
+//     else {
+//         cout<< "Number are printed";
+//     }
+// }
+// int main (){
+//     int n;
+//     cout << "Enter the number:";
+//     cin>>n;
+
+//     f(n);
+    
+// }
+
+#include <iostream>
 using namespace std;
-
-void f(int i,int n)
-{
-    if(i > n )
-    return;
-    cout << i<< endl;
-
-    f(i+1,n);
+int f(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    return n + f(n - 1);
 }
 
 int main (){
-    int n,i =1;
-    cin>>n;
+    int n ;
+    cout<<" Enter the Number :";
 
-    f(i,n);
-     return 0;
+    cin>> n;
+
+
+
+   int sum =f(n);
+cout << sum;
+
 }
