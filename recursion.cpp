aa9888b1a63f69eba7c10911bb6276393c1720eb
Akhil866 <akhilyadavpr430@gmail.com>
2025-08-28 00:16:22 +0530

    Recusion question 5 and 6

--- recursion.cpp
+++ recursion.cpp
@@ -73,8 +73,8 @@
     
 // }
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 // int f(int n) {
 //     if (n == 0) {
 //         return 0;
@@ -118,19 +118,88 @@ using namespace std;
 
 //functional 
 
-int f(int n )
+// int f(int n )
+// {
+//     if(n == 0){
+//         return 0;
+//     }
+//     return n + f(n-1);
+// }
+
+// int main ()
+// {
+//     int n ;
+//     cin>> n;
+
+//     cout<<f(n)<<endl;
+//     return 0;
+// }
+
+
+
+
+// reverse the array by recusion 
+
+// #include <iostream>
+// using namespace std;
+
+// void f (int i , int arr[], int n)
+// {
+//     if ( i >= n/2 ){
+//         return;
+//     }
+//      swap(arr[i] , arr[n-i-1]);
+
+//      f(i+1, arr , n);
+
+// }
+// //
+
+// int main (){
+
+//     int n ;
+//     cout<< "Enter the number :";
+//     cin>> n;
+
+//     int arr[n];
+
+//     for (int i = 0 ; i < n ; i++){ 
+//     cout<< "Enter the element:";
+//      cin >> arr[i];
+//     }
+//    f(0 , arr , n);
+
+//     for (int i = 0 ; i < n ; i++) cout << arr[i] << " ";
+
+// }
+
+
+
+#include <iostream>
+using namespace std;
+
+bool f(int n , string s)
 {
-    if(n == 0){
-        return 0;
+    if (n >= s.size()/2)
+    {
+        return true;
     }
-    return n + f(n-1);
+    else if (s[n] != s[s.size() - n - 1])
+    {
+        return false;
+    }
+
+    else {
+        return f(n+1 , s);
+    }
+
 }
 
-int main ()
-{
-    int n ;
-    cin>> n;
+int main (){
+    string s = "MADA";
 
-    cout<<f(n)<<endl;
+    cout<<f(0 , s);
     return 0;
+
 }
+
